6b4f4b3dc17c2bde9edb16c125d1cb9a603809ad
mustafatavasli <musstafatavasli@gmail.com>
2024-07-23 10:59:38 +0300

    Initial Commit

---
+++ Filmler/FilmlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmlerApp.swift
+//  Filmler
+//
+//  Created by Mustafa TAVASLI on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FilmlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
